050f8a5df2e2f2a799ac45cbd9e312f71caa542a
userxx1000 <u202118264@upc.edu.pe>
2025-07-27 18:01:39 -0500

    fix: name database

--- backend/config/config.js
+++ backend/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + "_test",
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
     port: process.env.DB_PORT || 3306
@@ -20,7 +20,7 @@ module.exports = {
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + "_prod",
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
     port: process.env.DB_PORT || 3306
