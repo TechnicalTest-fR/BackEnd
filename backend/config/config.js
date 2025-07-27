62865766a2b3c09bff76bc7f1ecfa84e402e095f
userxx1000 <u202118264@upc.edu.pe>
2025-07-27 18:09:38 -0500

    fix: port database

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 5000
   },
   test: {
     username: process.env.DB_USER,
@@ -15,7 +15,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 5000
   },
   production: {
     username: process.env.DB_USER,
@@ -23,6 +23,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 5000
   }
 };
