40c5627187f30ff3ead3a535aa6ddeee45c7f49a
userxx1000 <u202118264@upc.edu.pe>
2025-07-27 17:08:21 -0500

    feat: change configuration for deploy with other tool

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,35 +6,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "mssql", // <--- CAMBIAR AQUÍ a mssql para SQL Server
-    port: process.env.DB_PORT || 1433, // <--- AÑADIR PUERTO, por defecto 1433
-    dialectOptions: { // <--- AÑADIR OPCIONES ESPECÍFICAS PARA AZURE SQL
-      encrypt: true, // Usa cifrado SSL/TLS
-      trustServerCertificate: false // Para certificados de Azure SQL (true para autofirmados)
-    }
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME + "_test",
     host: process.env.DB_HOST,
-    dialect: "mssql", // <--- CAMBIAR AQUÍ
-    port: process.env.DB_PORT || 1433, // <--- AÑADIR PUERTO
-    dialectOptions: { 
-      encrypt: true,
-      trustServerCertificate: false
-    }
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME + "_prod",
     host: process.env.DB_HOST,
-    dialect: "mssql", // <--- CAMBIAR AQUÍ
-    port: process.env.DB_PORT || 1433, // <--- AÑADIR PUERTO
-    dialectOptions: { 
-      encrypt: true,
-      trustServerCertificate: false
-    }
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306
   }
 };
